8df4aa0b37e06c1a0216c996a72f6d3561b47e1f
Yacine Khatal <khatal.yacine@gmail.com>
2016-01-20 10:30:19 +0100

    Use default config if not setted

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+
+var defaultConfig = {
+  isLogEnabled: true,
+  amqpUrl: process.env.AMQP_URL || 'amqp://localhost',
+  prefetch: 1
+};
+
 module.exports = function(config) {
   return {
-    producer: require('./lib/producer')(config || {}),
-    consumer: require('./lib/consumer')(config || {})
+    producer: require('./lib/producer')(config || defaultConfig),
+    consumer: require('./lib/consumer')(config || defaultConfig)
   };
 };
