325cb8915a16c53ff752f3d49e0eaeb4de866a7e
Yacine Khatal <khatal.yacine@gmail.com>
2016-01-22 17:41:51 +0100

    Remove isValidUrl function and add getValidUrl

--- helpers/utils.js
+++ helpers/utils.js
@@ -1,11 +1,12 @@
 
-
-module.exports.isValidUrl = function(_url) {
-  if (_url && typeof _url === 'string' && (_url.indexOf('amqp://') === 0 || _url.indexOf('amqps://') === 0)) {
-    return true;
+module.exports.getValidUrl = function(_urls) {
+  for (var i = 0, l = _urls.length; i < l; ++i) {
+    if (_urls[i] && typeof _urls[i] === 'string' && (_urls[i].indexOf('amqp://') === 0 || _urls[i].indexOf('amqps://') === 0)) {
+      return _urls[i];
+    }
   }
 
-  return false;
+  return 'amqp://localhost';
 };
 
 module.exports.parseMsg  = function(_msg) {
