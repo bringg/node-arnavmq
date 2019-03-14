feea828855a25c7ddd0938e2137950e851ca7a4f
Alex Simenduev <shamil.si@gmail.com>
2019-03-14 22:23:17 +0200

    update node modules, and tidy up npm scripts

--- test/docker.js
+++ test/docker.js
@@ -4,7 +4,7 @@ const docker = process.env.CIRCLECI === 'true'
   ? 'ssh remote-docker \\ docker'
   : 'docker';
 
-module.exports.run = () => exec(`${docker}  run -d --name=rabbitmq-bunnymq -p 5672:5672 rabbitmq:3.6`);
+module.exports.run = () => exec(`${docker}  run -d --name=rabbitmq-bunnymq -p 5672:5672 rabbitmq:3.7`);
 module.exports.start = () => exec(`${docker} start rabbitmq-bunnymq`);
 module.exports.stop = () => exec(`${docker} stop rabbitmq-bunnymq`);
 module.exports.rm = () => exec(`${docker} rm -f rabbitmq-bunnymq || true`);
